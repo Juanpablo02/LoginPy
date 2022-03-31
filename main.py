6835d884e81d0f212c2302ebbd5a6ac931ff1e5b
svelezpernett <89142690+svelezpernett@users.noreply.github.com>
2022-03-31 07:46:44 -0500

    login con python

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hola Qatar')
+# Crear login usuario "admin" contra "aadmin123" y que dig verdadero, sino cuente las veces que se intento
+
+usuario = "admin"
+contraseña = "admin123"
+
+def login():
+    contador = 0
+    intentos = 0
+    while contador < 1:
+        usuario  = input("Usuario: ")
+        contraseña = input("contraseña: ")
+        if(usuario == "admin" and contraseña == "admin123"):
+            print("Verdadero")
+            contador = 2
+            print(intentos)
+        else:
+            intentos+=1
+print(login())
